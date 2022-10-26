da4364e4e76133fbe72511fe89a80da4a5634ab4
Azure Devops <joey.vagedes@gmail.com>
2022-10-26 14:26:11 -0700

    Update

--- .azurepipelines/script.py
+++ .azurepipelines/script.py
@@ -67,7 +67,7 @@ Automatically generated PR
 
         print('Updating the subtree')
         # Update the subtree, adds the commits to branch so no need to run commit command
-        r.git.subtree('pull', '--prefix', '.github/', 'https://github.com/Javagedes/mu_common_github', 'master')
+        r.git.subtree('pull', '--prefix', '.github/', f'https://{user}:{token}@github.com/Javagedes/mu_common_github', 'master')
 
         # Push the commit
         print("Pushing the commit")
