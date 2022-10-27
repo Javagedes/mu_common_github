b57784fa3813286f2411e299646c5fe78f0c7967
Azure Devops <joey.vagedes@gmail.com>
2022-10-27 11:37:33 -0700

    flake8 fix

--- .azurepipelines/scripts/subtree_pr_gen.py
+++ .azurepipelines/scripts/subtree_pr_gen.py
@@ -85,45 +85,55 @@ Automatically generated PR
         logging.info(f'Starting subtree update for {repo["name"]}.')
 
         logging.info('Searching for an open PR for updating the subtree.')
-        github_repo = github_wrapper.get_repo(f'{repo["url"].lstrip("https://github.com/").rstrip(".git")}')
+        github_repo = github_wrapper.get_repo(
+            f'{repo["url"].lstrip("https://github.com/").rstrip(".git")}')
         for pull in github_repo.get_pulls(state='open'):
             if pull.title == pr_title:
-                logging.info("PR found... closing the PR and deleting the branch.")
-                pull.edit(state='closed', body='Replaced by newer Subtree update.')
+                logging.info("PR found... closing the PR and deleting the \
+                              branch.")
+                pull.edit(state='closed', body='Replaced by newer Subtree \
+                                                update.')
                 ref = github_repo.get_git_ref(f'heads/{head}')
                 ref.delete()
 
         logging.info(f'Cloning {repo["name"]}')
-        r = Repo.clone_from(repo["url"], os.path.join(repo_base_path, repo["name"]))
+        r = Repo.clone_from(
+            repo["url"], os.path.join(repo_base_path, repo["name"]))
 
-        # This process is more complex then it should to be. The subtree pull 
+        # This process is more complex then it should to be. The subtree pull
         # command creates a merge commit on top of the subtree pull commit;
-        # however,a REBASE&FF cannot be performed on a PR if a merge commit 
+        # however,a REBASE&FF cannot be performed on a PR if a merge commit
         # is present. This is an issue because a REBASE&FF MUST be performed as
-        # we don't allow merge commits, and a squash changes commit hashes and 
+        # we don't allow merge commits, and a squash changes commit hashes and
         # breaks future subtree commands.
         #
         # To get around this, we perform the pull on a temporary branch and
-        # cherry-pick the second commit hash into the branch the PR will be 
+        # cherry-pick the second commit hash into the branch the PR will be
         # creating for. (The first commit is the merge commit and the second is
         # the subtree pull commit).
         logging.info('Performing the subtree pull')
         r.git.checkout('-b', 'temp')
-        r.git.subtree('pull', '--prefix', '.github/', 'https://github.com/Javagedes/mu_common_github', 'main', '--squash')
+        r.git.subtree('pull', '--prefix', '.github/',
+                      'https://github.com/Javagedes/mu_common_github',
+                      'main', '--squash')
+
         hash = r.git.log('-n', '1', '--skip=1', '--pretty=format:%H')
-        
+
         r.git.checkout(repo['base'])
         r.git.checkout('-b', head)
         r.git.cherry_pick(hash)
 
         # Push the commit
         logging.info('Pushing the Subtree pull commit to branch: {head}')
-        r.git.push(f'https://{user}:{token}@{repo["url"].lstrip("https://")}', head, "--force")
+        r.git.push(f'https://{user}:{token}@{repo["url"].lstrip("https://")}',
+                   head, "--force")
 
         # Create PR
-        logging.info(f'Creating the subtree update PR for repo: {repo["name"]}')
-        github_repo = github_wrapper.get_repo(f'{repo["url"].lstrip("https:://github.com/").rstrip(".git")}')
-        github_repo.create_pull(title=pr_title, body = pr_body, head = head, base = repo["base"])
+        logging.info(f'Creating the subtree update PR for repo:{repo["name"]}')
+        github_repo = github_wrapper.get_repo(
+            f'{repo["url"].lstrip("https:://github.com/").rstrip(".git")}')
+        github_repo.create_pull(
+            title=pr_title, body=pr_body, head=head, base=repo["base"])
 
 
 if __name__ == "__main__":
