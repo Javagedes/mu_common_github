92c958716cd25a8aa75f62c375bec8800f2ee983
Azure Devops <joey.vagedes@gmail.com>
2022-10-27 11:06:45 -0700

    Update

--- .azurepipelines/scripts/subtree_pr_gen.py
+++ .azurepipelines/scripts/subtree_pr_gen.py
@@ -1,5 +1,6 @@
 # @file subtree_pr_gen.py
-# For all provided repos, performs a subtree pull for a specified prefix and generates a pull request.
+# For all provided repos, performs a subtree pull for a specified prefix and
+# generates a pull request.
 #
 ##
 # Copyright (c) Microsoft Corporation
@@ -13,44 +14,53 @@ import os
 import yaml
 import logging
 
+
 def setup_logging():
     logging.basicConfig(level=logging.INFO)
 
+
 def parse_args():
-    parser = argparse.ArgumentParser(description="Creates a PR in each repo that uses this as a subtree.")
+    parser = argparse.ArgumentParser(description="Creates a PR in each repo \
+        that uses this as a subtree.")
     parser.add_argument('--token', '-T', dest="token",
-        help = "the github token credential to use for authentication.")
+                        help="the github token credential to use for \
+                              authentication.")
     parser.add_argument('--user', '-U', dest="user",
-        help = "the name of the user to associate the pull request with.")
-    parser.add_argument('--prefix', '-P', dest="prefix", 
-        help = "the name of the subdir the subtree is located at.")
+                        help="the name of the user to associate the pull \
+                              request with.")
+    parser.add_argument('--prefix', '-P', dest="prefix",
+                        help="the name of the subdir the subtree is located \
+                              at.")
     parser.add_argument('--repos', '-R', dest="repos",
-        help = "the path to a yaml file containing a list of repos. repos must contain name, url, and base")
+                        help="the path to a yaml file containing a list of \
+                              repos. repos must contain name, url, and base")
 
     args = parser.parse_args()
     return args
 
+
 def parse_yaml(path):
     try:
         with open(path) as f:
-            yaml_dict = yaml.load(f, Loader=yaml.SafeLoader) 
+            yaml_dict = yaml.load(f, Loader=yaml.SafeLoader)
     except Exception:
         logging.error(f'Failed to locate and parse {path}')
         raise
 
     return yaml_dict["repos"]
 
+
 def main():
     setup_logging()
     args = parse_args()
-    
+
     token = args.token
     user = args.user
     prefix = args.prefix
     repo_list = parse_yaml(args.repos)
-    
+
     pr_title = f'Update {prefix} subtree'
-    
+
     pr_body = f'''
 ## MUST COMPLETE PR WITH A REBASE&FF
 
@@ -69,13 +79,13 @@ Automatically generated PR
     repo_base_path = os.path.join(os.getcwd(), 'src')
     os.makedirs(repo_base_path, exist_ok=True)
 
-    github_wrapper = Github(token) # wrapper for communicating with github
+    github_wrapper = Github(token)  # wrapper for communicating with github
 
     for repo in repo_list:
         logging.info(f'Starting subtree update for {repo["name"]}.')
-        
-        logging.info(f'Searching for an open PR for updating the subtree.')
-        github_repo = github_wrapper.get_repo(f'{repo["url"].lstrip("https:://github.com/").rstrip(".git")}')
+
+        logging.info('Searching for an open PR for updating the subtree.')
+        github_repo = github_wrapper.get_repo(f'{repo["url"].lstrip("https://github.com/").rstrip(".git")}')
         for pull in github_repo.get_pulls(state='open'):
             if pull.title == pr_title:
                 logging.info("PR found... closing the PR and deleting the branch.")
@@ -114,6 +124,7 @@ Automatically generated PR
         logging.info(f'Creating the subtree update PR for repo: {repo["name"]}')
         github_repo = github_wrapper.get_repo(f'{repo["url"].lstrip("https:://github.com/").rstrip(".git")}')
         github_repo.create_pull(title=pr_title, body = pr_body, head = head, base = repo["base"])
+
 
 if __name__ == "__main__":
     main()
