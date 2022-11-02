f77f7618a026cebaf6488f28aca91f30b5bdb435
Azure Devops <joey.vagedes@gmail.com>
2022-11-02 08:39:56 -0700

    Update

--- .azurepipelines/scripts/subtree_pr_gen.py
+++ .azurepipelines/scripts/subtree_pr_gen.py
@@ -81,12 +81,12 @@ Automatically generated PR
 
     github_wrapper = Github(token)  # wrapper for communicating with github
 
-    for repo in repo_list:
-        logging.info(f'Starting subtree update for {repo["name"]}.')
+    for r in repo_list:
+        logging.info(f'Starting subtree update for {r["name"]}.')
 
         logging.info('Searching for an open PR for updating the subtree.')
         github_repo = github_wrapper.get_repo(
-            f'{repo["url"].lstrip("https://github.com/").rstrip(".git")}')
+            f'{r["url"].lstrip("https://github.com/").rstrip(".git")}')
         for pull in github_repo.get_pulls(state='open'):
             if pull.title == pr_title:
                 logging.info("PR found... closing the PR and deleting the \
@@ -96,50 +96,32 @@ Automatically generated PR
                 ref = github_repo.get_git_ref(f'heads/{head}')
                 ref.delete()
 
-        logging.info(f'Cloning {repo["name"]}')
-        r = Repo.clone_from(
-            repo["url"], os.path.join(repo_base_path, repo["name"]))
+        logging.info(f'Cloning {r["name"]}')
+        target_repo = Repo.clone_from(
+            r["url"], os.path.join(repo_base_path, r["name"]))
 
-        # This process is more complex then it should to be. The subtree pull
-        # command creates a merge commit on top of the subtree pull commit;
-        # however,a REBASE&FF cannot be performed on a PR if a merge commit
-        # is present. This is an issue because a REBASE&FF MUST be performed as
-        # we don't allow merge commits, and a squash changes commit hashes and
-        # breaks future subtree commands.
-        #
-        # To get around this, we perform the pull on a temporary branch and
-        # cherry-pick the second commit hash into the branch the PR will be
-        # creating for. (The first commit is the merge commit and the second is
-        # the subtree pull commit).
-        #logging.info('Performing the subtree pull')
-        #r.git.checkout('-b', 'temp')
-        #r.git.subtree('pull', '--prefix', '.github/',
-        #              'https://github.com/Javagedes/mu_common_github',
-        #              'main', '--squash')
-        #
-        #hash = r.git.log('-n', '1', '--skip=1', '--pretty=format:%H')
-        #
-        #r.git.checkout(repo['base'])
+        this_repo = Repo(os.getcwd())
+
+        this_repo.git.format_patch("-n", "HEAD^", "--stdout", ">", "temp.patch")
+        target_repo.git.patch("-p3", "temp.patch")
+
+
+
+
+        # This way works if we want to just filter out specific files.
+        #r.git.remote('add', 'github', 'https://github.com/Javagedes/mu_common_github.git')
+        #r.git.fetch('github')
         #r.git.checkout('-b', head)
-        #r.git.cherry_pick(hash)
-
-        # Push the commit
-        #logging.info('Pushing the Subtree pull commit to branch: {head}')
-        #r.git.push(f'https://{user}:{token}@{repo["url"].lstrip("https://")}',
-        #           head, "--force")
-        r.git.remote('add', 'github', 'https://github.com/Javagedes/mu_common_github.git')
-        r.git.fetch('github')
-        r.git.checkout('-b', head)
-        r.git.pull('--strategy', 'subtree', '--squash', 'github', 'main', '--allow-unrelated-histories')
-        r.git.commit('-m', '[.github] update')
-        r.git.push(f'https://{user}:{token}@{repo["url"].lstrip("https://")}', head, "--force")
+        #r.git.pull('--strategy', 'subtree', '--squash', 'github', 'main', '--allow-unrelated-histories')
+        #r.git.commit('-m', '[.github] update')
+        #r.git.push(f'https://{user}:{token}@{repo["url"].lstrip("https://")}', head, "--force")
 
         # Create PR
-        logging.info(f'Creating the subtree update PR for repo:{repo["name"]}')
+        logging.info(f'Creating the subtree update PR for repo:{r["name"]}')
         github_repo = github_wrapper.get_repo(
-            f'{repo["url"].lstrip("https:://github.com/").rstrip(".git")}')
+            f'{r["url"].lstrip("https:://github.com/").rstrip(".git")}')
         github_repo.create_pull(
-            title=pr_title, body=pr_body, head=head, base=repo["base"])
+            title=pr_title, body=pr_body, head=head, base=r["base"])
 
 
 if __name__ == "__main__":
