687fbb6e0b49c3c96d3dbba47f5b49871062b31f
Azure Devops <joey.vagedes@gmail.com>
2022-11-02 10:33:29 -0700

    update

--- .azurepipelines/scripts/subtree_pr_gen.py
+++ .azurepipelines/scripts/subtree_pr_gen.py
@@ -112,8 +112,12 @@ Automatically generated PR
         target_repo.git.fetch('github')
         target_repo.git.checkout('-b', head)
         target_repo.git.pull('--strategy', 'subtree', '--squash', 'github', 'main', '--allow-unrelated-histories')
+        logging.info(target_repo.git.status())
         for ignore_file in r["ignore"]:
-            logging.info(target_repo.git.reset("--", ignore_file))
+            logging.info(ignore_file)
+            target_repo.git.reset("--", ignore_file)
+
+        logging.info(target_repo.git.status())
         target_repo.git.commit('-m', '[.github] update')
         target_repo.git.push(f'https://{user}:{token}@{r["url"].lstrip("https://")}', head, "--force")
 
